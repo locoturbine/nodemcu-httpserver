e9add5f63480c81a4ad185a8cf89dba7ac69baaa
Cia, Federico <Federico.Cia@cmegroup.com>
2016-12-14 15:32:02 -0600

    update lua nd html

--- http/roomba_controller.lua
+++ http/roomba_controller.lua
@@ -1,50 +1,39 @@
--- garage_door_opener.lua
+-- roomba_controller.lua
 -- Part of nodemcu-httpserver, example.
--- Author: Marcos Kirsch
+-- Author: Federico Cia
 
 local function sendCommand(connection, command)
 
--- Replace with serial communication to roomba
     print('Configuring serial port to roomba.. ')
     uart.alt('1')
-   uart.setup(0,115200,8,0,1)
-   tmr.delay(200 * 1000) -- 1 seconds
-   uart.write(0,128)
-   tmr.delay(100 * 1000) -- 500 ms
-   uart.write(0,131)
-   tmr.delay(100 * 1000) -- 500 ms
-   
-   uart.write(0,command)
-   uart.alt(0)
---    gpio.write(pin, gpio.LOW)
---    gpio.mode(pin, gpio.OUTPUT)
---    gpio.write(pin, gpio.LOW)
---    tmr.delay(300000) -- in microseconds
---    gpio.write(pin, gpio.HIGH)
---    gpio.mode(pin, gpio.INPUT)
+    uart.setup(0,115200,8,0,1)
+    tmr.delay(200 * 1000) -- 1 seconds
+    uart.write(0,128)
+    tmr.delay(100 * 1000) -- 100 ms
+--    uart.write(0,131)
+--    tmr.delay(100 * 1000) -- 100 ms
 
-   -- Send back JSON response.
-   connection:send("HTTP/1.0 200 OK\r\nContent-Type: application/json\r\nCache-Control: private, no-store\r\n\r\n")
-   connection:send('{"error":0, "message":"OK"}')
+    sep = ","
+    for str in string.gmatch(command, "([^"..sep.."]+)") do
+        uart.write(0,command)
+        tmr.delay(100 * 1000) -- 100 ms
+    end
+
+    uart.alt(0)
+
+    -- Send back JSON response.
+    connection:send("HTTP/1.0 200 OK\r\nContent-Type: application/json\r\nCache-Control: private, no-store\r\n\r\n")
+    connection:send('{"error":0, "message":"OK"}')
 
 end
 
 return function (connection, req, args)
-   print('Sending command to roomba: ' .. args.command)
-   if  args.command -= nil  then 
+    print('Sending command to roomba: ' .. args.command)
+    if  args == nil or args.command == nil then
+        connection:send("HTTP/1.0 400 OK\r\nContent-Type: application/json\r\nCache-Control: private, no-store\r\n\r\n")
+        connection:send('{"error":-1, "message":"Bad command"}')
+    else
         sendCommand(connection, args.command)   -- Start Command
---    elseif args.action == "stop" then sendCommand(connection, 128)   -- Stop Command
---    elseif args.action == "dock" then sendCommand(connection, 143)   -- Stop Command
---    elseif args.action == "spot" then sendCommand(connection, 134)   -- Stop Command
---    elseif args.action == "poweroff" then sendCommand(connection, 133)   -- Stop Command
-   
---    elseif args.action == "forward" then sendCommand(connection, 134)   -- Stop Command
---    elseif args.action == "reverse" then sendCommand(connection, 134)   -- Stop Command
---    elseif args.action == "left" then sendCommand(connection, 134)   -- Stop Command
---    elseif args.action == "right" then sendCommand(connection, 134)   -- Stop Command
-   
-   else
-      connection:send("HTTP/1.0 400 OK\r\nContent-Type: application/json\r\nCache-Control: private, no-store\r\n\r\n")
-      connection:send('{"error":-1, "message":"Bad command"}')
-   end
+
+    end
 end
