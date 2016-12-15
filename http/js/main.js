19855684c79514ad02641d97b713028afa384c07
Federico Cia <fcia@iMac.local>
2016-12-15 10:30:57 -0600

    gyro working

--- http/js/main.js
+++ http/js/main.js
@@ -7,19 +7,19 @@ function init()
 		
 		window.addEventListener("deviceorientation", function(event) 
 		{
-			document.getElementById("y").style.webkitTransform = "scaleY("+(Math.round(event.beta))+")";  
-			document.getElementById("x").style.webkitTransform = "scaleX("+(Math.round(event.gamma))+")";
+			document.getElementById("x").style.webkitTransform = "scaleX("+(Math.round(event.beta))+")";  
+			document.getElementById("y").style.webkitTransform = "scaleY("+(Math.round(event.gamma))+")";
 			document.getElementById("angle").style.webkitTransform = "rotateZ("+(Math.round(event.alpha))+"deg)";
 		//	document.getElementById("gamma").innerHTML = Math.round(event.gamma);
 		//	document.getElementById("beta").innerHTML = Math.round(event.beta);
 
 			var betaDiff = Math.abs(Math.round(event.beta) - lastBeta )
-			if (betaDiff > 10 ) {
+			if (betaDiff >= 5 ) {
 				lastBeta = Math.round(event.beta)
 				document.getElementById("beta").innerHTML = Math.round(event.beta) + " : " + betaDiff;
 			}
 			var gammaDiff = Math.abs(Math.round(event.gamma) - lastGamma )
-			if (gammaDiff > 10 ) {
+			if (gammaDiff >= 5 ) {
 				lastGamma = Math.round(event.gamma)
 				document.getElementById("gamma").innerHTML = Math.round(event.gamma) + " : " + gammaDiff;
 
